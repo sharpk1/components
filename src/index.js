1f57519ab68e6d80dbc2cba172b0fba598ae47ec
kshah93 <kshah@lwolf.com>
2019-12-12 15:12:58 -0600

    more card reusability implementation

--- src/index.js
+++ src/index.js
@@ -2,33 +2,41 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
-import ApprovalCard from './ApprovalCard';
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-        <div></div>
-        <div></div>
-        <div></div>
-        <ApprovalCard/>
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:45 PM"
-        content={faker.lorem.sentence()}
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today at 2:00 AM"
-        content={faker.lorem.sentence()}
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Yesterday at 5:00 PM"
-        content={faker.lorem.sentence()}
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45 PM"
+          content={faker.lorem.sentence()}
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:00 AM"
+          content={faker.lorem.sentence()}
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 5:00 PM"
+          content={faker.lorem.sentence()}
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };

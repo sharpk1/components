273dc210810df079ba508ef14192aa136a9eba08
kshah93 <kshah@lwolf.com>
2019-12-12 14:54:19 -0600

    implementing card with approve/reject

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className="ui container comments">
+        <div></div>
+        <div></div>
+        <div></div>
+        <ApprovalCard/>
       <CommentDetail
         author="Sam"
         timeAgo="Today at 4:45 PM"
